ccfbba1e2fe320cc429c193667265b00b1073da4
lopeze25 <112291194+lopeze25@users.noreply.github.com>
2025-04-06 00:27:03 -0500

    Added matrix.cpp

---
+++ ParticleInInfiniteWell1D/Vector.cpp
@@ -0,0 +1,16 @@
+
+#include "Vector.h";
+
+double Vector::norm() const {
+
+}
+double Vector::dot(const Vector& secondOperand) const {
+
+}
+Vector Vector::cross(const Vector& secondOperand) const {
+
+}
+Vector Vector::normalize() const {
+
+}
+
