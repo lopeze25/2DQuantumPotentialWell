429c8065b8d054dc927cf6414dbf1b8a6e488d45
lopeze25 <112291194+lopeze25@users.noreply.github.com>
2025-04-09 16:44:04 -0500

    Added a test for a vector of vectors

--- ParticleInInfiniteWell1D/Source.cpp
+++ ParticleInInfiniteWell1D/Source.cpp
@@ -1,12 +1,40 @@
 #include <iostream>
 #include<stdio.h> 
-#include<GL/glut.h> 
+#include <vector>
+// #include<GL/glut.h> 
 #include<math.h> 
 
 int main() {
+      vector<vector<int>> a(3, vector<int>(3));
+
+        a[0][0] = 1;
+        a[0][1] = 2;
+        a[0][2] = 3;
+        a[1][0] = 4;
+        a[1][1] = 5;
+        a[1][2] = 6;
+        a[2][0] = 7;
+        a[2][1] = 8;
+        a[2][2] = 9;
+
+        for (int i = 0; i < 3; ++i) {
+            for (int j = 0; j < 3; ++j) {
+                cout << a[i][j] << " ";
+            }
+            cout << endl;
+        }
+    }
+
+	/*
+	
+		Schroingers equation
 
 
 
+
+	
+	
+	*/
 
 	/*
 	
