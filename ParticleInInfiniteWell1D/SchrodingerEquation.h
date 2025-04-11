9a8a4ee4808bef43833dd82c92fa0c57a28c50f7
lopeze25 <112291194+lopeze25@users.noreply.github.com>
2025-04-10 23:06:30 -0500

    Began work on schrodinger equation files

---
+++ ParticleInInfiniteWell1D/SchrodingerEquation.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "Matrix.h"
+
+class SchrodingerSolver {
+private:
+    int gridSizeX;
+    int gridSizeY;
+    double dx;
+    double dy;
+    double mass;
+    double hbar;
